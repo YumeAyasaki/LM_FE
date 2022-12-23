619a09b61749f22dfeb4539bfbca447051ae7d32
Huy đẹp trai <86175306+HuyHCMUS@users.noreply.github.com>
2022-12-23 17:59:39 +0700

    coponent Menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,99 @@
+<template>
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>QUẢN LÝ THƯ VIỆN</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <li>
+                    <a href="#">Tiếp nhận nhân viên</a>
+                </li>
+                <li>
+                    <a href="#">Lập thẻ độc giả</a>
+                </li>
+                <li>
+                    <a href="#">Tiếp nhận sách mới</a>
+                </li>
+                <li>
+                    <a href="#">Tra cứu sách</a>
+                </li>
+                <li>
+                    <a href="#">Mượn sách</a>
+                </li>
+                <li>
+                    <a href="#">Trả sách</a>
+                </li>
+                <li>
+                    <a href="#">Ghi nhận mất sách</a>
+                </li>
+                <li>
+                    <a href="#">Lập phiếu thu tiền phạt</a>
+                </li>
+                <li>
+                    <a href="#">Thanh lý sách</a>
+                </li>
+                <li>
+                    <a href="#">Báo cáo</a>
+                </li>
+                <li>
+                    <a href="#">Quản lý</a>
+                </li>
+            </ul>
+        </nav>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style>
+
+body {
+    font-family: 'Tahoma', sans-serif;
+    background: #fafafa;
+    margin: 0;
+    padding: 0;
+
+}
+a,
+a:hover,
+a:focus {
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.3s;
+}
+
+
+#sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    min-height: 100vh;
+    background: #b4b6be;
+    color: #fff;
+    transition: all 0.3s;
+    display: block;
+}
+
+#sidebar .sidebar-header {
+    padding: 15px;
+    background: #888889;
+}
+
+#sidebar ul.components {
+    padding: 20px 0;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+
+#sidebar ul li a:hover {
+    color: #000;
+    background: #fff;
+}
+
+</style>
